f2c38424df8727991fca64d4471d4ffd3d58be77
unknown <naiyume>
2020-05-14 15:14:06 -0400

    Updates

---
+++ precache-manifest.e4c9d80d63c30222ba3b3edd321be271.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1632562621931b080f2a4793f63cac",
+    "url": "/conceptApp/index.html"
+  },
+  {
+    "revision": "13fab7e97011af5a483f",
+    "url": "/conceptApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/conceptApp/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/conceptApp/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13fab7e97011af5a483f",
+    "url": "/conceptApp/static/js/main.e671d30a.chunk.js"
+  },
+  {
+    "revision": "3f2cbdd1ffd795d6e90f",
+    "url": "/conceptApp/static/js/runtime-main.374124cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/conceptApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
